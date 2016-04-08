451b37259c77b2581a77940cb6ffacb289faa052
Constantine Tretyak <Constantine Tretyak>
2016-04-08 14:21:28 +0300

    0.01

---
+++ src/org/fistashki/quicksort/algorithms/yours/QuickSort.java
@@ -0,0 +1,17 @@
+package org.fistashki.quicksort.algorithms.yours;
+
+import org.fistashki.quicksort.Sorting;
+
+/**
+ * Created by ctretyak on 08.04.2016.
+ */
+public class QuickSort implements Sorting {
+	public QuickSort() {
+	}
+
+	@Override
+	public int[] sort(int[] unsortedArray) {
+		// Ваш код здесь. Метод должен возвращать отсортированный unsortedArray.
+		return unsortedArray;
+	}
+}
